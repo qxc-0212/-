80bbc8fcf2ed3a5d3a4b10f812962df25f619225
qxc-0212 <77526514+qxc-0212@users.noreply.github.com>
2021-02-05 16:27:07 +0800

    Update 模拟实现memcpy.c

--- 模拟实现memcpy.c
+++ 模拟实现memcpy.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-//ģ��ʵ��memcpy
+//Ä£ÄâÊµÏÖmemcpy
 void teyp_memmcpy(void* de1, const void* de2, size_t count)
 {
 	void* ret = de1;
-	assert(de1&&de2);//����
+	assert(de1&&de2);//¶ÏÑÔ
 	while (count--)
 	{
 		*(char*)de1 = *(char*)de2;
@@ -25,6 +25,48 @@ void test1()
 int main()
 {
 	test1();
+	system("pause");
+	return 0;
+}
+
+//模拟实现memmove
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+//模拟实现memmove
+void* my_memmove(void* deat, const void* det, size_t count)
+{
+	void* ret = deat;
+	assert(deat&&det);//断言
+	if (deat < det)//前到后
+	{
+		while (count--)
+		{
+			*(char*)deat = *(char*)det;
+			++(char*)deat;
+			++(char*)det;
+		}
+	}
+	else//后到前
+	{
+		while (count--)
+		{
+			*((char*)deat + count) = *((char*)det + count);
+		}
+	}
+	return ret;
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	my_memmove(arr + 3, arr, 16);
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 	system("pause");
 	return 0;
 }
